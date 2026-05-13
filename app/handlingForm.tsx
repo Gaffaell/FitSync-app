2654187cf05950397e8eb8c6fb9160cda63210cc
Rafael Siqueira <rafaelgiansiqueira@gmail.com>
2026-05-13 18:18:49 -0300

    feat: total integração com o banco e as telas de aluno do adm/ falta exercicios

--- app/handlingForm.tsx
+++
@@ -1,5 +0,0 @@
-export default function verifyCredentials(data: any) {
-  // Implement your credential verification logic here
-  console.log("Form data:", data);
-  alert("Credentials verified!");
-}
